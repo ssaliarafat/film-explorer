d535500f757a317ac91e8ace4d60f44e3a7a59a4
ssaliarafat <marvelarafat@gmail.com>
2026-06-12 17:49:06 +0300

    Add vite entry points week 6

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
-
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        movie: resolve(__dirname, "src/movie/index.html"),
+        favorites: resolve(__dirname, "src/favorites/index.html"),
+      },
+    },
   },
 });
